2ce3c7a362cea0c906d8ad7bd2c242124ac22002
Bueno <zymbabueno@gmail.com>
2017-05-18 18:22:51 -0300

    instalação e setup básico de vue-router, vue-resource e vuex

--- site/src/main.js
+++ site/src/main.js
@@ -1,17 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+Vue.use(Vuex)
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
 import App from './App'
-import router from './router'
+import routes from './router'
+import VuexStore from './vuex/store'
 
 require('bulma/css/bulma.css')
 
 Vue.config.productionTip = false
+Vue.http.options.root = process.env.SERVER
+
+const store = new Vuex.Store(VuexStore)
+
+const router = new VueRouter({
+  routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
